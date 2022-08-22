a2b3152490cf07fedb16fdd09dcd58e648e7cc04
Danny Elfanbaum <drelfanbaum@gmail.com>
2022-08-22 15:22:43 -0400

    add easier movement between windows for nvim

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,7 +1,10 @@
 vim.api.nvim_set_keymap('','<Esc>', ':noh<CR>', { noremap = true })
+
+-- fzf and Rg
 vim.api.nvim_set_keymap('','<C-p>', ':Files<CR>', { noremap = true })
--- I need the C-g to see the full filepath sometimes. :Rg is sufficient
---vim.api.nvim_set_keymap('','<C-g>', ':Rg<CR>', { noremap = true })
+vim.api.nvim_set_keymap('','<C-F>', ':Rg<CR>', { noremap = true })
+
+-- text movement
 vim.api.nvim_set_keymap('','k', 'gk', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('','<Up>', 'gk', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i','<Up>', '<C-o>gk', 
@@ -12,3 +15,9 @@ vim.api.nvim_set_keymap('i','<Down>', '<C-o>gj',
                         { noremap = true, silent = true })
 vim.api.nvim_set_keymap('','0', 'g0', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('','$', 'g$', { noremap = true, silent = true })
+
+-- window movement
+vim.api.nvim_set_keymap('','<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('','<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('','<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('','<C-l>', '<C-w>l', { noremap = true, silent = true })
