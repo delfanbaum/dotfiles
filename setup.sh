0184ad8374569ebf35d6e04accda0decccb55bd8
Danny Elfanbaum <drelfanbaum@gmail.com>
2022-10-05 14:49:45 -0400

    abort setup for now

--- setup.sh
+++
@@ -1,7 +0,0 @@
-echo "Copying and/or updating dotfiles..."
-
-cp -vnrup $(pwd)/nvim ~/.config
-cp -vnp $(pwd)/vim/.vimrc ~/.vimrc
-cp -vnrp $(pwd)/omzsh/. ~/
-cp -vnrp $(pwd)/tmux/. ~/
-cp -vnrp $(pwd)/alacritty ~/.config
