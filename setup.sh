8cb7b9778d71d92c2243038911e4f1e24f5a8dbf
Danny Elfanbaum <drelfanbaum@gmail.com>
2022-10-05 14:32:17 -0400

    fix paths for linux

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo "Copying and/or updating dotfiles..."
 
-cp -vnrup ./nvim ~/.config
-cp -vnp ./vim/.vimrc ~/.vimrc
-cp -vnrp ./omzsh/. ~/
-cp -vnrp ./tmux/. ~/
-cp -vnrp ./alacritty ~/.config
+cp -vnrup nvim ~/.config
+cp -vnp vim/.vimrc ~/.vimrc
+cp -vnrp omzsh/. ~/
+cp -vnrp tmux/. ~/
+cp -vnrp alacritty ~/.config
