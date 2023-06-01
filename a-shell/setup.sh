e399c57e153a706afa007402d6ac767d7e942212
delfanbaum <drelfanbaum@gmail.com>
2023-06-01 10:38:38 -0400

    Improvments to setup script

--- a-shell/setup.sh
+++ a-shell/setup.sh
@@ -1,12 +1,10 @@
-# ensure we're starting in the "home" directory
-cd
-
-echo "Configuring a-shell colors..."
+echo "Configuring a-shell..."
 config -b "#282c34" -p  # background color
 config -f "#abb2bf" -p  # foreground color
 config -k beam -p  # cursor as beam
 
-echo "Setting up vim plugins..."
+echo "Setting up vim..."
+cp .vimrc ~/Documents/.vimrc
 
 packages=("joshdick/onedark.vim" "tpope/vim-surround" "junegunn/goyo.vim" "habamax/vim-asciidoctor")
 
@@ -15,5 +13,7 @@ mkdir -p .vim/pack/vendor/start
 for p in ${packages[@]}; do
     echo "Installing $p..."
     path=${p##*/}
-    lg2 clone --depth 1 https://github.com/$p .vim/pack/vendor/start/$path
+    lg2 clone --depth 1 https://github.com/$p ~/Documents/.vim/pack/vendor/start/$path
 done
+
+echo "All set!"
