3ef43cb40653ed5f0863d6edd3416b63ea314ea8
delfanbaum <delfanbaum@oreilly.com>
2023-07-27 16:15:42 -0400

    Add conditional wordcount to lualine

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -3,11 +3,24 @@ if not status_ok then
 	return
 end
 
+-- add word count
+local function word_count()
+    local fts = {asciidoctor = true,
+                 text = true,
+                 markdown = true,
+                 asciiboc = true}
+    if fts[vim.bo.filetype] then
+        return tostring(vim.fn.wordcount().words) .. " w"
+    else
+        return ''
+    end
+end
+
+
 lualine.setup {
   options = {
       icons_enables = true,
       theme = 'onedark',
-      -- globalstatus = true,
       component_separators = {
           left = '|',
           right = '|'
@@ -17,11 +30,14 @@ lualine.setup {
       lualine_a = {{ 'mode', fmt = function(str) return str:sub(1,1) end }},
       lualine_b = {'branch', 'diff', 'diagnostics'},
       lualine_c = {'filename'},
-      lualine_x = {'encoding',
+      lualine_x = { word_count,
+                    'encoding',
                     {
                         'filetype',
-                        icon_only = true,
-                        colored = false
+                        icon_only = false,
+                        colored = false,
+                        icon = { align = 'right' },
+                        padding = { left = 1, right = 2}
                     }
                   },
       lualine_y = {'progress'},
--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -15,7 +15,7 @@ return require('packer').startup(function()
     use 'neanias/everforest-nvim' -- for "light mode"
     use {
         'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        requires = { 'kyazdani42/nvim-web-devicons' }
     }
 
     -- Git things
