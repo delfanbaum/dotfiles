b75e86ea430c92680e3a0432b0116a477e21412a
delfanbaum <delfanbaum@oreilly.com>
2024-03-21 15:12:27 -0400

    reformat neotest

--- nvim/lua/config/neotest.lua
+++ nvim/lua/config/neotest.lua
@@ -1,6 +1,6 @@
 local status_ok, neotest = pcall(require, "neotest")
 if not status_ok then
-	return
+    return
 end
 
 -- adapters
@@ -9,25 +9,26 @@ local golang = require "neotest-go"
 
 neotest.setup({
     adapters = {
-	golang({
-		args = {"-count=1", "-timehout=60s"}
-	}),
+        golang({
+            args = { "-count=1", "-timehout=60s" }
+        }),
         python({
             runner = "pytest",
-            dap = { justMyCode = false,
-                    console = "integratedTerminal"
+            dap = {
+                justMyCode = false,
+                console = "integratedTerminal"
             }
         })
     },
     output = {
-      enabled = true,
-      open_on_run = false, -- until the quickfix issue is solved
-      short = false,
-      last_run = true,
-      auto_close = true
+        enabled = true,
+        open_on_run = false, -- until the quickfix issue is solved
+        short = false,
+        last_run = true,
+        auto_close = true
     },
     output_panel = {
-      enabled = true,
+        enabled = true,
     },
     floating = {
         max_height = 0.6,
