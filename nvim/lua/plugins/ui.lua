b974bee0f40dcd4dcefca45eb31feebcb0c3212d
delfanbaum <delfanbaum@oreilly.com>
2024-06-03 16:46:18 -0400

    Try another keybinding for fzf

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,17 +1,18 @@
 return {
-    {'nvim-lualine/lualine.nvim',
-     dependencies = { 'kyazdani42/nvim-web-devicons' }
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'kyazdani42/nvim-web-devicons' }
     },
     'lewis6991/gitsigns.nvim',
     "folke/zen-mode.nvim",
     "folke/twilight.nvim",
 
-    {'preservim/nerdtree',
-        lazy = true,
-        cmd = {'Nerdtree', 'NERDTreeToggle'},
-        dependencies = {
-            'Xuyuanp/nerdtree-git-plugin',
-            'ryanoasis/vim-devicons'
-        }
-    }
+    -- {'preservim/nerdtree',
+    --     lazy = true,
+    --     cmd = {'Nerdtree', 'NERDTreeToggle'},
+    --     dependencies = {
+    --         'Xuyuanp/nerdtree-git-plugin',
+    --         'ryanoasis/vim-devicons'
+    --     }
+    -- }
 }
